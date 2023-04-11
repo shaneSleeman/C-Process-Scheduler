98530cc4934639feed353d786d7414b2245d2106
shaneSleeman <shanesleeman1@gmail.com>
2023-04-11 11:22:58 +1000

    Testing RR readyprocess

--- allocate.c
+++ allocate.c
@@ -167,7 +167,7 @@ void scheduler(Process processes[], int processCount, int memoryChoice, int quan
                                     &maxOverhead, &totalOverhead);
 
                 if(memoryChoice) {
-                    
+                    /*
                     for(int i = 0; i < processCount; i++) {
                         if(totalTime - quantum >= lowestMultiple(
                                     processes[i].arrival, quantum) &&
@@ -179,8 +179,8 @@ void scheduler(Process processes[], int processCount, int memoryChoice, int quan
                                     processes[i].name, processes[i].memoryStart);
                             processes[i].started = 1;
                         }
-                    }
-                    //readyProcess(processCount, totalTime, quantum, memory, processes, sjf, 1);
+                    }*/
+                    readyProcess(processCount, totalTime, quantum, memory, processes, sjf, 1, &readyTime, &printedReady);
                 }
                 
                 printf("%d,FINISHED,process_name=%s,proc_remaining=%d\n", 
